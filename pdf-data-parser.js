5b9bbabde28ac61f6d158bb54ef03e96a6f8cfc1
drewlab <drew@dictadata.org>
2024-04-10 21:32:03 -0500

    read options file, support RepeatCell and RepeatHeading transforms

--- pdf-data-parser.js
+++ pdf-data-parser.js
@@ -5,33 +5,37 @@
  */
 "use strict";
 
-const PdfDataParser = require("./lib/PdfDataParser.js");
 const PdfDataReader = require("./lib/PdfDataReader.js");
-const RowAsObjects = require("./lib/RowAsObjects.js");
+const RepeatCellTransform = require("./lib/RepeatCellTransform.js");
+const RepeatHeadingTransform = require("./lib/RepeatHeadingTransform.js");
+const RowAsObjectTransform = require("./lib/RowAsObjectTransform.js");
 const FormatCSV = require("./lib/FormatCSV.js");
 const FormatJSON = require("./lib/FormatJSON.js");
 const Package = require("./package.json");
 var colors = require('colors');
 
-const fs = require('node:fs/promises');
+const { open, readFile } = require('node:fs/promises');
 const { pipeline } = require('node:stream/promises');
 const { stdout } = require('node:process');
 
 colors.enable();
 
-// set program argument defaults
+// default program options
 var options = {
-  url: ""
+  url: "",
+  format: "json",
+  output: "",
+  cells: 1,
+  lineHeight: 1.67,
+  orderXY: true
 }
-var outfile = "";
-var format = "json";
 
 /**
  * parseArgs
  *   only filename is required
- *   example ["node.exe", "pdf-data-parser.js", <pdf-filename>, <output-filename> "--cells=3", "--heading=title", "--repeating" "--headers=c1,c2,.." "--json|csv" ]
+ *   example ["node.exe", "pdf-data-parser.js", <filename.pdf|URL>, <output> "--cells=3", "--heading=title", "--repeating" "--headers=c1,c2,.." "--json|csv" ]
  */
-function parseArgs() {
+async function parseArgs() {
 
   let i = 2;
   while (i < process.argv.length) {
@@ -41,45 +45,49 @@ function parseArgs() {
       if (!options.url)
         options.url = arg;
       else
-        outfile = arg;
+        options.output = arg;
     }
     else {
       let nv = arg.split('=');
-      if (nv[ 0 ] === "--cells")
+
+      if (nv[ 0 ] === "--options") {
+        Object.assign(options, JSON.parse(await readFile(nv[ 1 ])));
+      }
+      else if (nv[ 0 ] === "--cells")
         options.cells = parseInt(nv[ 1 ]);
-      if (nv[ 0 ] === "--pages")
-        options.pages = nv[ 1 ].split(",");
+      else if (nv[ 0 ] === "--pages")
+        options.pages = nv[ 1 ];
       else if (nv[ 0 ] === "--heading")
         options.heading = nv[ 1 ];
-      else if (nv[ 0 ].includes("headers"))
+      else if (nv[ 0 ].includes("--headers"))
         options.headers = nv[ 1 ].split(",");
       else if (nv[ 0 ].startsWith("--repeating"))
         options.repeatingHeaders = true;
       else if (nv[ 0 ] === "--csv")
-        format = "csv";
+        options.format = "csv";
       else if (nv[ 0 ] === "--raw")
-        format = "raw";
+        options.format = "raw";
     }
     ++i;
   }
 
-  // convert pages arg
-  // from an array of strings
-  // to an array of integers
-  if (options.pages) {
-    let pgnums = []
+  if (typeof options.pages === "string") {
+    // convert pages arg
+    let pages = options.pages.split(",")
+    options.pages = []
 
-    for (let p of options.pages) {
+    for (let p of pages) {
       let range = p.split("-");
-      if (range.length === 1)
-        pgnums.push(parseInt(range[ 0 ]));
+      if (range.length === 1) {
+        // single page
+        options.pages.push(parseInt(range[ 0 ]));
+      }
       else {
+        // expand range into individual pages
         for (let i = parseInt(range[ 0 ]); i <= parseInt(range[ 1 ]); i++)
-          pgnums.push(i);
+          options.pages.push(i);
       }
     }
-
-    options.pages = pgnums;
   }
 }
 
@@ -89,11 +97,11 @@ function parseArgs() {
 (async () => {
   let retCode = 0;
 
-  parseArgs();
-  let consoleOn = outfile !== "" || !options.url;
+  await parseArgs();
+  let consoleOn = options.output !== "" || !options.url;
 
   if (consoleOn) {
-    console.log("pdfdataparser (pdp) " + Package.version);
+    console.log("pdp PDF Data Parser " + Package.version);
     console.log("Copyright 2024 Drew O. Letcher | The MIT License");
   }
 
@@ -101,38 +109,57 @@ function parseArgs() {
     console.log("");
     console.log("Parse tabular data from a PDF file.");
     console.log("");
-    console.log("pdp <filename.pdf|URL> [<output-file>] [--cells=#] [--heading=title], [--repeating] [--headers=name1,name2,...] [--csv|--json]");
+    console.log("pdp [--options=filename.json] <filename.pdf|URL> [<output>] [--cells=#] [--heading=title], [--repeating] [--headers=name1,name2,...] [--csv|--json]");
     console.log("");
+    console.log("  --options    - file containing JSON object with pdp options, optional.");
     console.log("  filename|URL - path name or URL of PDF file to process, required.");
-    console.log("  output-file  - local path name for output of parsed data, default stdout.");
+    console.log("  output       - local path name for output of parsed data, default stdout.");
     console.log("  --cells      - minimum number of cells for a data row, default = 1.");
     console.log("  --heading    - text of heading to find in document that precedes desired data table, default none.");
     console.log("  --headers    - comma separated list of column names for data, default none first table row contains names.")
     console.log("  --repeating  - table header row repeats on each PDF page, default = false.");
     console.log("  --csv        - output data in CSV format.");
     console.log("  --json       - output data in JSON format, default.");
-    console.log("  --raw        - output text in the document as JSON array of arrays.")
+    console.log("  --raw        - output text in the document as JSON array of arrays (rows).")
     console.log("");
     return;
   }
 
   try {
 
+    let pipes = [];
+
     let reader = new PdfDataReader(options);
-    let transform = new RowAsObjects(options);
-    let formatter = format === "csv" ? new FormatCSV() : new FormatJSON();
+    pipes.push(reader);
+
+    if (Object.hasOwn( options,  "RepeatCell.index") || Object.hasOwn( options, "index")) {
+      let transform = new RepeatCellTransform(options);
+      pipes.push(transform);
+    }
+
+    if (Object.hasOwn( options, "RepeatHeading.header") || Object.hasOwn( options, "header")) {
+      let transform = new RepeatHeadingTransform(options);
+      pipes.push(transform);
+    }
+
+    if (options.format !== "raw") {
+      let transform = new RowAsObjectTransform(options);
+      pipes.push(transform);
+    }
+
+    let formatter = options.format === "csv" ? new FormatCSV() : new FormatJSON();
+    pipes.push(formatter);
+
     let writer;
-    if (outfile) {
-      let fd = await fs.open(outfile, "w");
+    if (options.output) {
+      let fd = await open(options.output, "w");
       writer = fd.createWriteStream();
     }
     else
       writer = process.stdout;
+    pipes.push(writer);
 
-    if (format === "raw")
-      await pipeline(reader, formatter, writer);
-    else
-      await pipeline(reader, transform, formatter, writer);
+    await pipeline(pipes);
 
     writer.end();
   }
